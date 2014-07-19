ced0d3135e17714dfb30f36f5436eac4a3ed51a9
Jeffrey Hann <jeffhann@gmail.com>
2014-07-19 11:05:12 -0700

    finished "beer oclock" mode

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ TextLayer *text_time_layer;
 TextLayer *text_beer_layer;
 TextLayer *text_hours_layer;
 TextLayer *text_white_layer;
+TextLayer *text_drinkup_layer;
 Layer *line_layer;
 
 void line_layer_update_callback(Layer *layer, GContext* ctx) {
@@ -23,6 +24,7 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   static char hour_remaining_text[] = "00";
   static char beer_text[] = "Beer O'Clock";
   static char beer_text_remaining[] = "Hours Left";
+  static char drink_up[] = "Drink Up!";
 
   char *time_format;
 
@@ -42,26 +44,31 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   }
   
   strftime(time_text, sizeof(time_text), time_format, tick_time);
-  strftime(hour_remaining_text_temp, sizeof(hour_remaining_text_temp), "%H", tick_time);
-  int hour_remaining = atoi(hour_remaining_text_temp);
-  
-  if (hour_remaining <= 17) {
-    hour_remaining = 17 - hour_remaining;
-    snprintf(hour_remaining_text_temp, 100, "%d", hour_remaining);
-  }
-  
-  strcpy(hour_remaining_text, hour_remaining_text_temp);
   
   // Kludge to handle lack of non-padded hour format string
   // for twelve hour clock.
   if (!clock_is_24h_style() && (time_text[0] == '0')) {
     memmove(time_text, &time_text[1], sizeof(time_text) - 1);
   }
-
+  
   text_layer_set_text(text_time_layer, time_text);
-  text_layer_set_text(text_countdown_layer, hour_remaining_text);
+  
+  strftime(hour_remaining_text_temp, sizeof(hour_remaining_text_temp), "%H", tick_time);
+  int hour_remaining = atoi(hour_remaining_text_temp);
   text_layer_set_text(text_beer_layer, beer_text);
-  text_layer_set_text(text_hours_layer, beer_text_remaining);
+  
+  if (hour_remaining <= 17) {
+    hour_remaining = 17 - hour_remaining;
+    snprintf(hour_remaining_text_temp, 100, "%d", hour_remaining);
+    text_layer_set_text(text_hours_layer, beer_text_remaining);
+    strcpy(hour_remaining_text, hour_remaining_text_temp);
+    text_layer_set_text(text_countdown_layer, hour_remaining_text);
+    layer_set_hidden((Layer *)text_drinkup_layer, true);
+  } else {
+    text_layer_set_text(text_drinkup_layer, drink_up);
+    layer_set_hidden((Layer *)text_hours_layer, false);
+  }
+  
 }
 
 void handle_deinit(void) {
@@ -101,6 +108,13 @@ void handle_init(void) {
   text_layer_set_background_color(text_beer_layer, GColorClear);
   text_layer_set_font(text_beer_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(text_beer_layer));
+
+  text_drinkup_layer = text_layer_create(GRect(25, 103, 90, 70));
+  text_layer_set_text_alignment(text_drinkup_layer, GTextAlignmentCenter);
+  text_layer_set_text_color(text_drinkup_layer, GColorBlack);
+  text_layer_set_background_color(text_drinkup_layer, GColorClear);
+  text_layer_set_font(text_drinkup_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  layer_add_child(window_layer, text_layer_get_layer(text_drinkup_layer));
 
   text_countdown_layer = text_layer_create(GRect(0, 107, 144, 35));
   text_layer_set_text_alignment(text_countdown_layer, GTextAlignmentCenter);
